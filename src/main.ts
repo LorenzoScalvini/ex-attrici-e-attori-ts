7edf643c003e95015d149a3c0af0b72ebeea992d
LorenzoScalvini <lorenzo.scalvini1704@gmail.com>
2025-04-08 16:18:11 +0200

    Milestone 01

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { Persona } from "./types";
+
+const primapersona: Persona = {
+    id: 1,
+    name: "Pointcrow",
+    annoNascita: 1999,
+    annoMorte: 2024,
+    biografia: "Biografia",
+    img: "persona foto.jpg"
+};
+
+console.log(primapersona);
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export type Persona = {
+    readonly id: number;
+    readonly name: string;
+    annoNascita: number;
+    annoMorte: number;
+    biografia: string;
+    img: string;
+}
